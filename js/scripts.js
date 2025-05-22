18c29033d76960bab171a8749f5cd88148bd337e
Bubliikkk <polatovaan223@gmail.com>
2025-05-22 16:30:00 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно')
 });
+const intensiveImg = document.querySelector(".answers__title");
+intensiveImg.addEventListener('mouseenter', () => {
+    console.log('Мышка наведена на изображение, показываем текст');
+});
+

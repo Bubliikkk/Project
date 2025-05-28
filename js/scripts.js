9adf2046abe4590ea33a3a892ebf9f1f1a942786
Bubliikkk <polatovaan223@gmail.com>
2025-05-28 17:23:24 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -28,5 +28,48 @@ intensiveImg.forEach((item, index) => {
       intensiveText[index].setAttribute('hidden', true);
     });
 });
+// Кнопка прокрутки вверх
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.getElementById("scrollToTop");
+
+    // Показываем кнопку при прокрутке вниз
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+
+    // Прокрутка вверх при клике
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
+//Объявляем переменную ReviewsContainer и сохраняем в нее элементы reviews
+const TReviewsContainer = document.querySelector(".reviews");
+
+//  проверяем существует ли элемент ReviewsContainer, если он существует то переходим далее
+    if (TReviewsContainer) {
+//далее создаем массив dataTitleReviews, который содержит строки с именами тренеров.(здесь уже пишем те значения, которые надо подставить вместо слова ученик 1, ученик 2)
+        const dataTitleReviews = [
+            "Ирина Лайм",
+            "Марина Орлова",
+            "Максим Петров",
+
+
+        ];
+
+//Объявляем переменную titleReviews и сохраняем в нее все элементы на странице с классом reviews__subtitle (где должны стоять имена)
+        const titleReviews =
+            TReviewsContainer.querySelectorAll(".reviews__subtitle");
+
+// Проходим по каждому элементу массива titleTreners с помощью цикла forEach. Внутри функции 2 переменные: item – текущий заголовок, а index — его индекс в массиве.
+            titleReviews.forEach((item, index) => {
+
+//здесь обновляем значение текущего заголовка (textContent) на новое значение из массива dataTitleCards, используя индекс текущего заголовка.
+            item.textContent = dataTitleReviews[index];
+           });
+    }
 
 
